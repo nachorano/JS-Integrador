c0fc881c6ed7686bde6cd74d9ea2e0651eaae4bf
nachorano <ignacioranod@gmail.com>
2022-10-24 18:15:59 -0300

    Actualizado

---
+++ assets/js/storage.js
@@ -0,0 +1,19 @@
+const productsData = [
+    {
+      id: 1,
+      name: 'cherry quick',
+      decription: 'Quick Detail con aroma a cereza, formulado para otorgar un brillo único.',
+      capacity: '600 ML',
+      category: 'quickDetail',
+      cardImg: './assets/img/products/Cherru_Quick.png',
+    },
+    {
+        id: 1,
+        name: 'Golden Messi',
+        bid: 6.89,
+        user: 'thetroncous',
+        category: 'futbol',
+        userImg: './assets/img/user.png',
+        cardImg: './assets/img/products/goldenmessi.png',
+      }
+]
