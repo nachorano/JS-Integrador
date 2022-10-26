7954cc911106b14289ab0ec37d5b1f2348834fb8
nachorano <ignacioranod@gmail.com>
2022-10-26 18:57:25 -0300

    editando storge.js

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -1,19 +1,173 @@
 const productsData = [
-    {
-      id: 1,
-      name: 'cherry quick',
-      decription: 'Quick Detail con aroma a cereza, formulado para otorgar un brillo único.',
-      capacity: '600 ML',
-      category: 'quickDetail',
-      cardImg: './assets/img/products/Cherru_Quick.png',
-    },
-    {
-        id: 1,
-        name: 'Golden Messi',
-        bid: 6.89,
-        user: 'thetroncous',
-        category: 'futbol',
-        userImg: './assets/img/user.png',
-        cardImg: './assets/img/products/goldenmessi.png',
-      }
-]
+  {
+    id: 1,
+    name: "cherry quick",
+    decription:
+      "Quick Detail con aroma a cereza, formulado para otorgar un brillo único.",
+    capacity: "600 ML",
+    category: "quickDetail",
+    productImg: "./assets/img/products/Cherru_Quick.png",
+  },
+  {
+    id: 2,
+    name: "extreme detail",
+    description:
+      "Quick Detail con aroma a durazno, formulado para otorgar un brillo único. Puede ser utilizado en superficies húmedas y secas.",
+    capacity: "600 ML",
+    category: "quickDetail",
+    productImg: "./assets/img/products/extreme_detail.png",
+  },
+  {
+    id: 3,
+    name: "american shine",
+    description:
+      "Ideal para remover fácilmente el polvillo diario, restos de cera y pulimiento. Otorga un excelente brillo sobre la pintura.",
+    capacity: "600 ML",
+    category: "quickDetail",
+    productImg: "./assets/img/products/american_shine.png",
+  },
+  {
+    id: 4,
+    name: "waterless",
+    description:
+      "Limpiador en seco formulado para suspender la suciedad del vehículo y poder realizar una limpieza sin necesidad de utilizar agua. Contiene agentes lubricantes y ceras.",
+    capacity: "600 ML",
+    category: "quickDetail",
+    productImg: "./assets/img/products/waterless.png",
+  },
+  {
+    id: 5,
+    name: "ilussion wax",
+    description:
+      "Es un Quick Detail elaborado con un gran porcentaje de cera de carnauba, la cual otorga un excelente brillo con efecto húmedo y protege la superficie del vehículo.",
+    capacity: "600 ML",
+    category: "cera",
+    productImg: "./assets/img/products/illusion_wax.png",
+  },
+  {
+    id: 6,
+    name: "CARNAUBA PURE WAX",
+    description:
+      "Cera en pasta no abrasiva compuesta por cera de carnauba y cera de abeja con aroma a ananá.",
+    capacity: "90 GR",
+    category: "cera",
+    productImg: "./assets/img/products/cera_pure.png",
+  },
+  {
+    id: 7,
+    name: "THE BO$$ SHINE",
+    description:
+      "Sellador de pintura.             Su fórmula de polímeros siliconados, fluoro-polímeros y resinas nos aseguran el máximo brillo combinados con una repelencia al agua y suavidad al tacto.",
+    capacity: "600 ML",
+    category: "sellador",
+    productImg: "./assets/img/products/boss.png",
+  },
+  {
+    id: 8,
+    name: "LAVA CRUSH",
+    description:
+      "Sellador diseñado para proteger la pintura del vehículo. Formula de polímeros siliconados y resinas que aseguran brillo.",
+    capacity: "600 ML",
+    category: "sellador",
+    productImg: "./assets/img/products/crush.png",
+  },
+  {
+    id: 9,
+    name: "SEAL IT ALL",
+    description:
+      "Sellador de carácter polifuncional capaz de sellar cualquier tipo de superficie tales como pinturas, vidrios, llantas, plásticos y cromados.",
+    capacity: "600 ML",
+    category: "sellador",
+    productImg: "./assets/img/products/seal_it_all.png",
+  },
+  {
+    id: 10,
+    name: "atomic",
+    description:
+      "Shampoo de PH ácido con agradable aroma a cítrico. Ideal para realizar lavados pre-tratamientos.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/atomic.png",
+  },
+  {
+    id: 11,
+    name: "banana",
+    description:
+      "Shampoo con potenciador de brillo, PH neutro, alto poder espumógeno y de limpieza con aroma a banana.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/banana.png",
+  },
+  {
+    id: 12,
+    name: "wax",
+    description:
+      "Shampoo con PH neutro, alto poder espumógeno y de limpieza con aroma a cereza. Contiene carnauba y no barre ceras ni selladores siendo seguro para superficies con tratamientos.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/wax.png",
+  },
+  {
+    id: 13,
+    name: "supreme",
+    description:
+      "Shampoo generador de espuma consistente con aroma a frutilla. Posee carnauba que otorga protección, tiene PH neutro y no barre ceras ni selladores.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/supreme.png",
+  },
+  {
+    id: 14,
+    name: "pure foam",
+    description:
+      "Shampoo diseñado para Foam Lance. Posee PH neutro, alto poder espumógeno y de limpieza. No barre ceras ni selladores.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/pure_foam.png",
+  },
+  {
+    id: 15,
+    name: "elite",
+    description:
+      "Shampoo que posee un mix de ceras de máxima calidad y potenciadores de brillo con aroma a coco y banana. PH neutro.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/elite_shampoo.png",
+  },
+  {
+    id: 16,
+    name: "dip club",
+    description:
+      "Shampoo revolucionario que en su composición posee un sellador ideal para vehículos con ploteo vinílico o líquido. Contiene PH neutro.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/dip_club.png",
+  },
+  {
+    id: 17,
+    name: "hyper black",
+    description:
+      "Producto estrella de la marca, posee un gran contenido de sílice que potencia el brillo y sella la superficie. PH neutro y alto poder espumógeno.",
+    capacity: "600 ML",
+    category: "shampoo",
+    productImg: "./assets/img/products/hyper_black.png",
+  },
+  {
+    id: 18,
+    name: "INFERNO GEL",
+    description:
+      "APC Premium desarrollado para los más exigentes detailers del mercado.",
+    capacity: "600 ML",
+    category: "limpiadores",
+    productImg: "./assets/img/products/inferno_gel.png",
+  },
+  {
+    id: 19,
+    name: "falta editar",
+    description:
+      "APC Premium desarrollado para los más exigentes detailers del mercado.",
+    capacity: "600 ML",
+    category: "limpiadores",
+    productImg: "./assets/img/products/inferno_gel.png",
+  },
+];
